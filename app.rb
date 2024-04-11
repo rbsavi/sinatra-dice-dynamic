7635c5c238afc962330102032b8784bf934b1b5b
rbsavi <164175444+rbsavi@users.noreply.github.com>
2024-04-11 22:47:08 +0000

    dice

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ get("/") do
   erb(:homepage)
 end
 
-
 get("/dice/:number_of_dice/:how_many_sides") do
   @num_dice = params.fetch("number_of_dice").to_i
   @sides = params.fetch("how_many_sides").to_i
